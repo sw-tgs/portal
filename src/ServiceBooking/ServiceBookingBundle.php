b15833d0a9d5daf5c025f1e0e05230fc411987bb
Stephan Pohl <s.pohl@shopware.com>
2020-03-25 15:11:27 +0100

    Initial import of the ServiceBookingBundle

---
+++ src/ServiceBooking/ServiceBookingBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Shopware\Production\ServiceBooking;
+
+use Shopware\Core\Framework\Bundle;
+
+class ServiceBookingBundle extends Bundle
+{
+    protected $name = 'ServiceBooking';
+}
