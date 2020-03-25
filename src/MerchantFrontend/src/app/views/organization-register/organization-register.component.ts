ef7f8f03eab354c87bd6ba818d43e0886f079e62
Oliver Terhaar <o.terhaar@shopware.com>
2020-03-25 17:46:11 +0100

    First implementation of Hubspot formular

--- src/MerchantFrontend/src/app/views/organization-register/organization-register.component.ts
+++ src/MerchantFrontend/src/app/views/organization-register/organization-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from "../../../environments/environment";
 
 @Component({
   selector: 'portal-organization-register',
@@ -10,6 +11,14 @@ export class OrganizationRegisterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let dashboardElem = document.getElementById('hubspot-form');
+
+    if (dashboardElem && dashboardElem.childElementCount === 0) {
+      let hubspotScript = document.createElement('script');
+      hubspotScript.innerHTML = `hbspt.forms.create({portalId: "6506294",formId: "e2d4a12e-a1c0-4ac0-87b5-ed89c490ecff"});`;
+      dashboardElem.appendChild(hubspotScript);
+    }
+
   }
 
 }
